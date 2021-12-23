f1a8a34ca64fcb4000629007214ea74c44319bb5
0x20F <0x20fa@gmail.com>
2021-12-23 15:06:58 +0100

    Add a config to keep track of things between commands. A database would've been overkill

---
+++ src/config.rs
@@ -0,0 +1,63 @@
+use serde::{ Serialize, Deserialize };
+use std::collections::HashMap;
+use std::path::Path;
+use std::fs;
+use crate::error::{ Result, CarbonError };
+
+
+
+
+static CONFIG_PATH: &'static str = "/tmp/carbon-footprint.toml";
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    running: HashMap<String, Vec<String>>
+}
+
+impl Default for Config {
+    fn default() -> Config {
+        Self {
+            running: HashMap::new()
+        }
+    }
+}
+
+impl Config {
+    pub fn get() -> Self {
+        // Create a fresh struct if it's not already written to file
+        match fs::read_to_string(CONFIG_PATH) {
+            Ok(s) => toml::from_str(&s).unwrap(),
+            Err(_) => Self::default()
+        }
+    }
+
+
+    pub fn save(config: &Config) -> Result<()> {
+        let content = toml::to_string(config).unwrap();
+        fs::write(CONFIG_PATH, &content).expect("Couldn't save config file");
+
+        Ok(())
+    }
+
+
+    pub fn get_running_services(&self) -> &HashMap<String, Vec<String>> {
+        &self.running
+    }
+
+
+    pub fn set_running_services(&mut self, services: HashMap<String, Vec<String>>) {
+        self.running = services;
+    }
+
+
+    pub fn add_running_service(&mut self, path: &str, services: Vec<&str>) {
+        self.running.insert(
+            path.to_string(),
+            services
+                .iter()
+                .map(|s| String::from(*s))
+                .collect()
+        );
+    }
+}

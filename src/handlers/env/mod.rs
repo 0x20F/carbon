9a2c52ac85ea0ce56406f23090a27987b9d969e8
0x20F <0x20fa@gmail.com>
2021-12-26 15:41:19 +0100

    fix: Improve handler readability. Less indentation is good.

--- src/handlers/env/mod.rs
+++ src/handlers/env/mod.rs
@@ -7,37 +7,38 @@ use crate::config::Footprint;
 /// Handler function for every parameter
 /// and subcommand of the `env` command
 pub fn handle(matches: &ArgMatches) -> Result<()> {
-    if let Some(matches) = matches.subcommand_matches("env") {
-        let mut config = Footprint::get();
-
-
-        if let Some(matches) = matches.subcommand_matches("add") {
-            let path: String = matches.value_of("path").unwrap().to_string();
-            let id: String = matches.value_of("identifier").unwrap().to_string();
-
-            config.add_env_file(&path, &id);
-            Footprint::save(&config)?;
-        }
-
-        if let Some(matches) = matches.subcommand_matches("remove") {
-            let id: String = matches.value_of("identifier").unwrap().to_string();
-            
-            config.remove_env_file(&id);
-            Footprint::save(&config)?;
-        }
-
-        if let Some(_) = matches.subcommand_matches("list") {
-            config.print_as_table();
-        }
-
-        if let Some(matches) = matches.subcommand_matches("activate") {
-            let id: String = matches.value_of("identifier").unwrap().to_string();
-            
-            config.activate_env_file(&id);
-            Footprint::save(&config)?;
-        }
+    if let None = matches.subcommand_matches("env") {
+        return Ok(());
     }
 
+    let matches = matches.subcommand_matches("env").unwrap();
+    let mut config = Footprint::get();
+
+    if let Some(matches) = matches.subcommand_matches("add") {
+        let path: String = matches.value_of("path").unwrap().to_string();
+        let id: String = matches.value_of("identifier").unwrap().to_string();
+
+        config.add_env_file(&path, &id);
+        Footprint::save(&config)?;
+    }
+
+    if let Some(matches) = matches.subcommand_matches("remove") {
+        let id: String = matches.value_of("identifier").unwrap().to_string();
+        
+        config.remove_env_file(&id);
+        Footprint::save(&config)?;
+    }
+
+    if let Some(_) = matches.subcommand_matches("list") {
+        config.print_as_table();
+    }
+
+    if let Some(matches) = matches.subcommand_matches("activate") {
+        let id: String = matches.value_of("identifier").unwrap().to_string();
+        
+        config.activate_env_file(&id);
+        Footprint::save(&config)?;
+    }
 
     Ok(())
 }
--- src/handlers/services/mod.rs
+++ src/handlers/services/mod.rs
@@ -10,41 +10,44 @@ use crate::docker;
 /// Handler function for every parameter
 /// and subcommand of the `service` command.
 pub fn handle(matches: &ArgMatches) -> Result<()> {
-    if let Some(service_matches) = matches.subcommand_matches("service") {
-        let _ = docker::running()?;
-        let mut service_handler = Service::new();
+    if let None = matches.subcommand_matches("service") {
+        return Ok(());
+    }
+
+    let _ = docker::running()?;
+    let service_matches = matches.subcommand_matches("service").unwrap();
+    let mut service_handler = Service::new();
+
+    if let Some(start_matches) = service_matches.subcommand_matches("start") {
+        let services: Vec<_> = start_matches.values_of("services").unwrap().collect();
+        let display = start_matches.is_present("display");
+
+        service_handler.start(services, display)?;
+    }
     
-        if let Some(start_matches) = service_matches.subcommand_matches("start") {
-            let services: Vec<_> = start_matches.values_of("services").unwrap().collect();
-            let display = start_matches.is_present("display");
-    
-            service_handler.start(services, display)?;
-        }
-        
-        if let Some(stop_matches) = service_matches.subcommand_matches("stop") {
-            let services: Vec<_> = stop_matches.values_of("services").unwrap().collect();
-            service_handler.stop(services)?;
-        }
+    if let Some(stop_matches) = service_matches.subcommand_matches("stop") {
+        let services: Vec<_> = stop_matches.values_of("services").unwrap().collect();
+        service_handler.stop(services)?;
+    }
 
-        if let Some(_) = service_matches.subcommand_matches("list") {
-            docker::container::show_all();
-        }
+    if let Some(_) = service_matches.subcommand_matches("list") {
+        docker::container::show_all();
+    }
 
-        if let Some(rebuild_matches) = service_matches.subcommand_matches("rebuild") {
-            let services: Vec<_> = rebuild_matches.values_of("services").unwrap().collect();
-            service_handler.rebuild(services)?;
-        }
+    if let Some(rebuild_matches) = service_matches.subcommand_matches("rebuild") {
+        let services: Vec<_> = rebuild_matches.values_of("services").unwrap().collect();
+        service_handler.rebuild(services)?;
+    }
 
-        if let Some(add_matches) = service_matches.subcommand_matches("add") {
-            let network: String = add_matches.value_of("network").unwrap().to_string();
-            let services: Vec<_> = add_matches.values_of("services").unwrap().collect();
+    if let Some(add_matches) = service_matches.subcommand_matches("add") {
+        let network: String = add_matches.value_of("network").unwrap().to_string();
+        let services: Vec<_> = add_matches.values_of("services").unwrap().collect();
 
-            // Start services
-            service_handler.start(services.clone(), false)?;
+        // Start services
+        service_handler.start(services.clone(), false)?;
 
-            // Add them to the network
-            docker::network::connect(&network, &services)?;
-        }
+        // Add them to the network
+        docker::network::connect(&network, &services)?;
     }
 
     Ok(())

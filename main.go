d539a913c56bdb1ff031a7a44be72635c95d39dc
0x20F <0x20fa@gmail.com>
2022-02-06 16:25:30 +0100

    What's next list

--- main.go
+++ main.go
@@ -11,6 +11,14 @@ func main() {
 	data, _ := yaml.Marshal(declarations["intake"].FullContents)
 	fmt.Println(string(data))*/
 
+	// Next Steps:
+	// 1. User provides list of carbon defined services to the program
+	// 2. Each of those services will get an injected container_name
+	// 3. All relevant services get merged into their own docker compose file
+	// 4. That docker compose file gets saved under a random name
+	// 5. Docker compose file gets started up
+	// 6. All service data gets pushed to the DB if compose file has started without issues
+
 	containers := database.Containers()
 	fmt.Println(containers)
 

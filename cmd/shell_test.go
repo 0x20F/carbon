7df18d9d5e6d9b793ca01ed6d5cd076c0cb392b8
0x20F <0x20fa@gmail.com>
2022-02-18 18:59:08 +0100

    Split big function into smaller ones

--- cmd/shell_test.go
+++ cmd/shell_test.go
@@ -107,3 +107,62 @@ func TestGenerateCommandReturnsEmptyWhenContainerNotFound(t *testing.T) {
 		t.Error("generateCommand should return an empty string when the container is not found")
 	}
 }
+
+func TestByCarbonFindsByServiceName(t *testing.T) {
+	beforeShellTest()
+
+	// Add some containers to the database
+	containers := []types.Container{
+		{
+			Image:       "image1",
+			Name:        "container1",
+			Uid:         "uid1",
+			ServiceName: "service1",
+		},
+		{
+			Image:       "image2",
+			Name:        "container2",
+			Uid:         "uid2",
+			ServiceName: "service2",
+		},
+	}
+
+	for _, container := range containers {
+		database.AddContainer(container)
+	}
+
+	// Generate the command with the service name
+	command := byCarbon("service1")
+
+	if command == "" {
+		t.Error("byCarbon should return a command when given a service name")
+	}
+}
+
+func TestByDockerFindsByUid(t *testing.T) {
+	beforeShellTest()
+
+	// Get all the generated containers first so we can get the uid
+	container := docker.RunningContainers()[0]
+
+	// Generate the command with the Uid
+	command := byDocker(container.Uid)
+
+	if command == "" {
+		t.Error("byDocker should return a command when given a Uid")
+	}
+}
+
+func TestByDockerFindsByName(t *testing.T) {
+	beforeShellTest()
+
+	// Get all the generated containers first so we can get the name
+	container := docker.RunningContainers()[0]
+
+	// Generate the command with the name
+	command := byDocker(container.Name)
+
+	if command == "" {
+		t.Error("byDocker should return a command when given a name")
+	}
+}
